7d01fc0b120c7378a7bc77a138dd30959f94a41f
harika-sigmoid <77790036+harika-sigmoid@users.noreply.github.com>
2021-07-24 19:43:06 +0530

    Add files via upload

---
+++ mmdet/Func/test.py
@@ -0,0 +1,75 @@
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory,make_response,Response
+from werkzeug.utils import secure_filename
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import pandas as pd
+
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
+DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
+ALLOWED_EXTENSIONS = {'pdf', 'txt'}
+
+app = Flask(__name__, static_url_path="/static")
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+# limit upload size upto 8mb
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            print('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('index.html')
+
+
+def process_file(path, filename):
+    remove_watermark(path, filename)
+    # with open(path, 'a') as f:
+    #    f.write("\nAdded processed content")
+
+
+def remove_watermark(path, filename):
+    input_file = PdfFileReader(open(path, 'rb'))
+    output = PdfFileWriter()
+    for page_number in range(input_file.getNumPages()):
+        page = input_file.getPage(page_number)
+        page.mediaBox.lowerLeft = (page.mediaBox.getLowerLeft_x(), 20)
+        output.addPage(page)
+    output_stream = open(app.config['DOWNLOAD_FOLDER'] + filename, 'wb')
+    output.write(output_stream)
+   
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    name=filename.replace('.pdf','.csv')
+    df = pd.DataFrame({'name': ['Raphael', 'Donatello'],
+                       'mask': ['red', 'purple'],
+                       'weapon': ['sai', 'bo staff']})
+    resp = make_response(df.to_csv())
+    resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    resp.headers["Content-Type"] = "text/csv"
+    return Response(
+       df.to_csv(),
+       mimetype="text/csv",
+       headers={"Content-disposition":
+       "attachment; filename=results"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
