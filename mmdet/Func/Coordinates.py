3984532e7524132c3d3bc237cf14986b34870c39
harika-sigmoid <77790036+harika-sigmoid@users.noreply.github.com>
2021-07-24 20:00:00 +0530

    Update Coordinates.py

--- mmdet/Func/Coordinates.py
+++ mmdet/Func/Coordinates.py
@@ -20,26 +20,26 @@ def model_test(image):
     return result
 
 def coordinates(path,file,ind):
-  from PIL import Image
-  from PyPDF2 import PdfFileReader 
-  from pdf2image import convert_from_path
-  import decimal   
-  result = model_test(path)
-  im = Image.open(path,mode='r')
-  x=im.size
-  input1 = PdfFileReader(open(file, 'rb'))
-  rect = input1.getPage(ind).mediaBox
-  twod_list = []
-  for i in range(len(result[0][2])):
-    x1 = result[0][2][i][0]
-    y1 = result[0][2][i][1]
-    x2 = result[0][2][i][2]
-    y2 = result[0][2][i][3]
+    from PIL import Image
+    from PyPDF2 import PdfFileReader 
+    from pdf2image import convert_from_path
+    import decimal   
+    result = model_test(path)
+    im = Image.open(path,mode='r')
+    x=im.size
+    input1 = PdfFileReader(open(file, 'rb'))
+    rect = input1.getPage(ind).mediaBox
+    twod_list = []
+    for i in range(len(result[0][2])):
+        x1 = result[0][2][i][0]
+        y1 = result[0][2][i][1]
+        x2 = result[0][2][i][2]
+        y2 = result[0][2][i][3]
 
-    xa = decimal.Decimal(float(x1)) * decimal.Decimal(float((rect[2]/x[0])))
-    ya = decimal.Decimal(float(rect[3])) - decimal.Decimal(float(y1)) * decimal.Decimal(float((rect[3]/x[1])))
-    xb = decimal.Decimal(float(x2)) * decimal.Decimal(float((rect[2]/x[0])))
-    yb = decimal.Decimal(float(rect[3])) - decimal.Decimal(float(y2)) * decimal.Decimal(float((rect[3]/x[1])))
-    new=str(xa)+','+str(ya)+','+str(xb)+','+str(yb)
-    twod_list.append(new)  
-  return twod_list  
+        xa = decimal.Decimal(float(x1)) * decimal.Decimal(float((rect[2]/x[0])))
+        ya = decimal.Decimal(float(rect[3])) - decimal.Decimal(float(y1)) * decimal.Decimal(float((rect[3]/x[1])))
+        xb = decimal.Decimal(float(x2)) * decimal.Decimal(float((rect[2]/x[0])))
+        yb = decimal.Decimal(float(rect[3])) - decimal.Decimal(float(y2)) * decimal.Decimal(float((rect[3]/x[1])))
+        new=str(xa)+','+str(ya)+','+str(xb)+','+str(yb)
+        twod_list.append(new)  
+    return twod_list  
